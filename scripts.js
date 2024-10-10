0a53f22f7003dce18ecd667f1ac4e8351802f58e
DanrleyMedeiros <danrley.medeiros@hotmail.com>
2024-10-10 16:59:11 -0300

    Alterações gerais

--- scripts.js
+++ scripts.js
@@ -1,16 +1,22 @@
 const form = document.querySelector(".formulario-fale-conosco")
 const mask = document.querySelector(".mascara-formulario")
+const voltaForm = document.querySelector(".volta-formulario")
 
 
 function chamaFormulario(){
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
     mask.style.visibility = "visible"
+    voltaForm.style.left = "-9%"
+
     
 }
 
-function ocultarMascaraFormulario(){
+
+function voltaFormulario(){
     mask.style.visibility = "hidden"
     form.style.left = "-22%"
     form.style.transform = "translateX(0%)"
+    voltaForm.style.left = "90%"
+    
 }
